8c9e7609c1e9f023b4332935155f2d67b191127c
Brian Hernández González <brianhernandezgonz96@gmail.com>
2024-11-13 22:20:00 +0000

    [spain91] Excuse generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let primero = "Un ";
+  let adjetivo = ["cartero ", "perro ", "loco ", "desarrollador ", "policia "];
+  let verbo = ["se comió mi ", "robó mi ", "quemó mi ", "congeló mi "];
+  let objeto = ["tarea ", "coche ", "móvil ", "ordenador ", "altavoz "];
+  let place = ["en mi casa", "en la calle", "en el trabajo", "en el parque"];
+
+  let word1 = Math.floor(Math.random() * adjetivo.length);
+  let word2 = Math.floor(Math.random() * verbo.length);
+  let word3 = Math.floor(Math.random() * objeto.length);
+  let word4 = Math.floor(Math.random() * place.length);
+
+  document.querySelector("#excuse").innerHTML =
+    primero + adjetivo[word1] + verbo[word2] + objeto[word3] + place[word4];
 };
